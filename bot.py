01c025e5f988dc44c736bf35d8fe3eeb6dfb9201
jenjnif <jenjnif@hotmail.com>
2018-04-05 21:00:08 +0100

    more behavior

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import pyjokes
+import datetime
 
 class Bot:
 	def public(self, channel, sender, msg):
@@ -6,17 +7,32 @@ class Bot:
 			return f'hello {sender}'
 		if "joke" in msg.lower():
 			return pyjokes.get_joke()
+		if "date" in msg.lower() or "day" in msg.lower():
+			d = datetime.datetime.now()
+			weekday = d.strftime('%A')
+			return week_days[weekday]
 
 
 	def private(self, sender, msg):
 		pass
 
+week_days = {'Monday': "Have a marvelous Monday", 'Tuesday': "Have a terrific Tuesday",
+'Wednesday': 'Have a wonderful Wednesday', 'Thursday': 'Have a Thabulous Thursday', 
+'Friday': 'Have a fantastic Friday', 'Saturday': 'Have a super Saturday',
+'Sunday': 'Have a sleepy Sunday'}
+
+def main():
+	bot = Bot()
+	msg = bot.public('ldnpydojo', 'Gautier', 'Hi')
+	if msg:
+		print(msg)
+	msg = bot.public('ldnpydojo', 'Gautier', 'tell me a joke')
+	if msg:
+		print(msg)
+	msg = bot.public('ldnpydojo', 'Gautier', 'what day is it?')
+	if msg:
+		print(msg)
 
 
-bot = Bot()
-msg = bot.public('ldnpydojo', 'Gautier', 'Hi')
-if msg:
-	print(msg)
-msg = bot.public('ldnpydojo', 'Gautier', 'tell me a joke')
-if msg:
-	print(msg)
+if __name__ == '__main__':
+	main()
